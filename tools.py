fix formula for F_n

tools.py
@@ -596,18 +596,10 @@ def build_Delta(ind_dict):
 def build_F_n(Delta, E_app):
     Delta_pos = Delta[:, 0].reshape((-1, 1))
     Delta_neg = Delta[:, 1].reshape((-1, 1))
-    alpha_pos = float(Delta_pos.T @ E_app @ Delta_pos)
-    alpha_neg = float(Delta_neg.T @ E_app @ Delta_neg)
-    alpha_pos_neg = float(- Delta_pos.T @ E_app @ Delta_neg)
-    #G = n**2 * Q - beta*np.eye(n)
-    #a11 = float(-Delta_neg.T @ G @ Delta_neg)
-    #a12 = float(Delta_pos.T @ G @ Delta_neg)
-    #a21 = float(Delta_pos.T @ G @ Delta_neg)
-    #a22 = float(-Delta_neg.T @ G @ Delta_neg)
     a11 = float(Delta_neg.T @ E_app @ Delta_neg)
     a12 = float(-Delta_pos.T @ E_app @ Delta_neg)
     a21 = float(-Delta_pos.T @ E_app @ Delta_neg)
-    a22 = float(Delta_neg.T @ E_app @ Delta_neg)
+    a22 = float(Delta_pos.T @ E_app @ Delta_pos)
     F_n = np.array([[a11, a12], [a21, a22]])
-    F_n = 1/(alpha_pos * alpha_neg - alpha_pos_neg**2) * F_n
+    F_n = 1/(a11 * a22 - a12*a21) * F_n
     return F_n