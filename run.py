correct formula for b_sqrt_n

run.py
@@ -30,15 +30,16 @@ plot_test = False
 """
 We iterate over a high number of experiences to better evaluate the orders.
 """
-nb_iter = 2
+nb_iter = 1
 h_lambdas_list = []
 h_b_list = []
 h_alpha_list = []
 h_alpha_app_list = []
 h_alpha_diff_list = []
 h_alpha_stds = []
+h_lambdas_list = []
 h_lambdas_app_list = []
-h_lambdas_app2_list = []
+h_lambdas_diff_list = []
 h_b_list = []
 h_b_app_list = []
 h_b_diff_list = []
@@ -68,10 +69,10 @@ for id_iter in range(nb_iter):
     print(f"cov_scal is {cov_scal}")
     #list_cardinals = [[32, 32, 96, 96]]
     #p_list = [512]
-    list_cardinals = [[16, 16, 48, 48], [32, 32, 96, 96], [64, 64, 192, 192]]
-    p_list = [256, 512, 1024]
-    #list_cardinals = [[42, 22, 66, 116], [84, 44, 132, 232]]
-    #p_list = [512, 1024]
+    #list_cardinals = [[16, 16, 48, 48], [32, 32, 96, 96], [64, 64, 192, 192]]
+    #p_list = [256, 512, 1024]
+    list_cardinals = [[42, 22, 66, 116], [84, 44, 132, 232]]
+    p_list = [512, 1024]
     #list_cardinals = [[150, 75, 75, 125], [300, 150, 150, 250], [600, 300, 300, 500]]
     #p_list = [256, 512, 1024]
     #list_cardinals = [[600, 300, 300, 500], [1200, 600, 600, 1000]]
@@ -103,7 +104,7 @@ for id_iter in range(nb_iter):
     alpha_diff_list = []
     lambdas_list = []
     lambdas_app_list = []
-    lambdas_app2_list = []
+    lambdas_diff_list = []
     b_list = np.array([])
     b_app_list = np.array([])
     b_diff_list = np.array([])
@@ -142,8 +143,9 @@ for id_iter in range(nb_iter):
                    mean_scal * one_hot(1, p), mean_scal * one_hot(1, p)]
         col = np.array([0.4**l for l in range(p)])
         C_2 = (1+5/np.sqrt(p))*sp_linalg.toeplitz(col, col.T)
-        cov_list = [np.eye(p), np.eye(p),
-                    C_2, C_2]
+        #cov_list = [np.eye(p), np.eye(p),
+        #            C_2, C_2]
+        cov_list = [np.eye(p), C_2, np.eye(p), C_2]
 
         ### For testing
         nb_loops = 10
@@ -180,6 +182,8 @@ for id_iter in range(nb_iter):
         b = sol_fair[0]
         lambda_pos = sol_fair[1]
         lambda_neg = sol_fair[2]
+        print(f"lambda_pos is {lambda_pos}")
+        print(f"lambda_neg is {lambda_neg}")
         alpha = sol_fair[3:]
         alpha = alpha.reshape((-1, 1))
         B_11 = matrix_fair[1:3, 1:3]
@@ -263,20 +267,24 @@ for id_iter in range(nb_iter):
         tilde_G_sqrt_n = build_tilde_F_n(Delta, mat_sqrt_n)
         det_tilde_G_n = np.linalg.det(tilde_G_n)
         det_tilde_F_n_app = np.linalg.det(tilde_F_n_app)
-
-        # Build the R_n vector
         factor = ones_k.T @ n_signed / n
-        R_n = 1/det_tilde_G_n * (2*gamma*f_p(tau))/(n*p) * tilde_G_n @ Delta.T @ J @ A_1_11 @ (n_signed - factor* vec_prop)
 
         ### Compute approximation of `b`
         b_sqrt_n = (1 + gamma*f(tau))/(gamma*f_p(tau)**2) * det_tilde_G_n + 1/p * t.T@J.T@Delta@tilde_G_n@Delta.T @ J @ t
         b_sqrt_n = 1/b_sqrt_n
         b_sqrt_n = b_sqrt_n * (
                 1/np.sqrt(p) * t.T @ J.T @ Delta @ tilde_G_n @ (
-                2*(1+gamma*f(tau))/(n*p) * Delta.T @ J @ A_1_11 @ (factor*vec_prop - n_signed) + gamma*f_p(tau)/2 * t.T @ n_signed * Delta.T @J@t)
+                2*(1+gamma*f(tau))/(n*p) * Delta.T @ J @ A_1_11 @ (factor*vec_prop - n_signed) + gamma*f_p(tau)/(n*p) * t.T @ n_signed * Delta.T @J@t)
                 - (1+gamma*f(tau))*det_tilde_G_n * t.T @ n_signed / (n*f_p(tau)*np.sqrt(p))
                 )
         b_app = ones_k.T @ n_signed / n + b_sqrt_n
+
+        ### Compute approximation of $\lambda_1, \lambda_{-1}$
+        # Build the R_n vector
+        R_n = gamma*f_p(tau)/det_tilde_G_n * tilde_G_n @ (
+                2/(n*p) * tilde_G_n @ Delta.T @ J @ A_1_11 @ (n_signed - factor* vec_prop)
+                + (gamma*f_p(tau)*t.T @ n_signed/(n*p) + b_sqrt_n/np.sqrt(p)) * Delta.T @ J @ t
+                )
         
         ### Compute approximation of `alpha`
         alpha_n_3_2 = - ( gamma/n * b_sqrt_n * 1/(1+gamma*f(tau))
@@ -331,7 +339,8 @@ for id_iter in range(nb_iter):
             expecs[a] += np.squeeze(tmp + D_cal + D_cal_x)
 
             ### Compute the variances `Var_a`
-            tmp = 2/p**2 * (gamma*f_pp(tau)/(2*n*np.sqrt(p))*t.T @ n_signed + n*alpha_n_3_2*f_p(tau))**2 * np.trace(cov_list[a] @ cov_list[a])
+            tmp = 2/p**2 * (gamma*f_pp(tau)/(2*n*np.sqrt(p))*t.T @ n_signed + n*alpha_n_3_2*f_p(tau))**2 * \
+                    np.trace(cov_list[a] @ cov_list[a])
             varis[a] += np.squeeze(tmp)
             tmp = (2*gamma*f_p(tau)/(n*p) * (n_signed - factor*vec_prop).T + 2*f_p(tau)/p*R_n.T@Delta.T@J) @ mu_diff.T @ cov_list[a] @ mu_diff @ (2*gamma*f_p(tau)/(n*p) * (n_signed - factor*vec_prop) + 2*f_p(tau)/p*J.T @ Delta @ R_n)
             varis[a] += np.squeeze(tmp)
@@ -354,6 +363,7 @@ for id_iter in range(nb_iter):
         """
         Store approximations
         """
+
         cumsum_cardinals = np.append(0, np.cumsum(cardinals))
         b_list = np.append(b_list, b)
         b_app_list = np.append(b_app_list, b_app)
@@ -361,6 +371,9 @@ for id_iter in range(nb_iter):
         alpha_list.append(np.copy(alpha))
         alpha_app_list.append(np.copy(alpha_app))
         alpha_diff_list.append(np.copy(alpha - alpha_app))
+        lambdas_list.append(np.array([lambda_pos, lambda_neg]))
+        lambdas_app_list.append(np.copy(R_n))
+        lambdas_diff_list.append(np.array([lambda_pos, lambda_neg]) - R_n)
     
 
         """
@@ -463,6 +476,10 @@ for id_iter in range(nb_iter):
     h_b_list.append(b_list)
     h_b_app_list.append(b_app_list)
     h_b_diff_list.append(b_diff_list)
+    h_lambdas_list.append(lambdas_list)
+    h_lambdas_app_list.append(lambdas_app_list)
+    h_lambdas_diff_list.append(lambdas_diff_list)
+
     # Build the standard deviation of the coefficient of `alpha`
     alpha_stds = []
     for a in range(len(cumsum_cardinals)):